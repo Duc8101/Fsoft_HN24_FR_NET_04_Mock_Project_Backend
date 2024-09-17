eea1bf25cbfd2245e63c05111547286afc7b8847
Duc8101 <vegitobasicinstict1810@gmail.com>
2024-09-17 16:13:04 +0700

    fix format code

--- HN24_FR_NET_04_Group_3/Phone_Shop.Common/DTOs/ProductDTO/ProductCreateUpdateDTO.cs
+++ HN24_FR_NET_04_Group_3/Phone_Shop.Common/DTOs/ProductDTO/ProductCreateUpdateDTO.cs
@@ -1,12 +1,12 @@
 namespace Phone_Shop.Common.DTOs.ProductDTO
 {
-  public class ProductCreateUpdateDTO
-  {
-    public string ProductName { get; set; } = null!;
-    public string Image { get; set; } = null!;
-    public decimal Price { get; set; }
-    public int CategoryId { get; set; }
-    public int Quantity { get; set; }
-    public string? Description { get; set; }
-  }
+    public class ProductCreateUpdateDTO
+    {
+        public string ProductName { get; set; } = null!;
+        public string Image { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public int Quantity { get; set; }
+        public string? Description { get; set; }
+    }
 }
